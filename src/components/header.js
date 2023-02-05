bb2bf3b7c9e86653cb42f27c248d0750f0397aed
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2023-02-05 21:08:37 +0100

    started header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+        <div className="header-img-container"></div>
+      <h1>
+        Hi 👋, my name is <span>Kathryn</span> and I'm a FrontEnd Web Developer
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
